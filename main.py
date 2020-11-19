25d81fbbf9baa837c316ca051bf18a5606b4387d
Gerlando30 <55295778+Gerlando30@users.noreply.github.com>
2020-11-19 18:12:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import leggifile
+import normalized_initialization
+import sigmoidal
+import Layer
+
+layer=3
+learning_rate=0.1
+output_expected=[4,8,3,2]
+num_epoch=400
+filename = "dati.csv"
+input_value=leggifile.leggi(filename)
+#creo la nuova struttura che conterrà i layer
+struct_layers = np.empty(layer,Layer.Layer)
+dim_input=np.size(input_value[0])
+nj= 4#np.random.randint(1,5)
+nj_next= 4#np.random.randint(1,5)
+x=np.zeros(dim_input)
+
+for i in range(np.size(struct_layers)):
+    
+    #inizializza i nostri layer e li stampo a schermo
+    struct_layers[i]=Layer.Layer(x,nj,nj_next,[np.size(x),nj])
+    x=np.zeros(nj)
+    nj=nj_next
+    #nj_next=np.random.randint(1,5)
+
+#out=sigmoidal.output_nn(struct_layers,input_value[0])
+#print("output expected :", output_expected)
+#print("expexted loss:",np.square(out-output_expected))   
+print("--------------------------------------")
+
+sigmoidal.backprogation(struct_layers,num_epoch,learning_rate,input_value[0],output_expected)
--- sigmoidal.py
+++ sigmoidal.py
@@ -1,50 +1,76 @@
 import numpy as np
-import matplotlib.pyplot as plt 
-import math 
+import matplotlib.pyplot as plt
+import math
 
-#funzione sigmoidale
+
+# funzione sigmoidale
 def sigmoid(x):
-    return np.divide(1, (1 +np.exp(-x)))
+    return np.divide(1, (1 + np.exp(-x)))
 
-#derivata funzione sigmoidale
+
+# derivata funzione sigmoidale
 def derivate_sigmoid(x):
     sig = sigmoid(x)
     return np.dot(sig, np.subtract(1, sig))
 
-#derivata loss
-def der_loss(output_layer,output_expected):
-    val=(output_layer-output_expected)
-    return np.dot(2,val)
 
-#funzione costo layer
-def dCdW(Di,Do,Dc):
-    val=np.dot(Di,Do)
-    val2=np.dot(val,Dc)
-    return val2
+# derivata loss
+def der_loss(output_layer, output_expected):
+    val = (output_expected - output_layer)
+    return np.dot(2, val)
+
+
+# funzione costo layer
+def c(Di, Do, Dc):
+    return 0
+
+
 
 def output_nn(struct_layers, x_input):
-    i=0
+    i = 0
     for layer in struct_layers:
         layer.x = x_input
-        print("input layer ",i,":",layer.x)
-        print("matrice peso layer ",i,":")
-        print(layer.w_matrix)
+        #print("input layer ", i, ":", layer.x)
+        #print("matrice peso layer ", i, ":")
+        #print(layer.w_matrix)
         x_input = layer.output()
-        i=i+1
+        i = i + 1
     return x_input
 
-def backprogation(struct_layers,num_epoch,learning_rate,output_NN,output_expected):
-    for epoch in range(0,num_epoch):
-        #scandisce tutti i layer presenti all'interno della rete
-        for i in range(np.size(struct_layers)-1,-1,-1):
-            #restituisce l'oggetto layer i-esimo
-            layer=struct_layers[i]
-            Di=layer.x
-            for j in range(0,layer.nj):
-                output=output_NN[j]
-                Do=derivate_sigmoid(output)
-                Dloss=der_loss(output,output_expected[j])
-                gradiente=dCdW(Di,Do,Dloss)
-                product=np.dot(learning_rate,gradiente)
-                layer.w_matrix[:,j]=np.add(layer.w_matrix[:,j],product)
-                
+
+def backprogation(struct_layers, num_epoch, learning_rate, x_input, output_expected):
+    for epoch in range(0, num_epoch):
+        output_NN = output_nn(struct_layers, x_input)
+        # scandisce tutti i layer presenti all'interno della rete
+        for i in range(np.size(struct_layers) - 1, -1, -1):
+            # restituisce l'oggetto layer i-esimo
+            layer = struct_layers[i]
+            Di = layer.x
+            # if output layer
+            delta = np.zeros(layer.nj)
+            if i == (np.size(struct_layers) - 1):
+                for j in range(0, layer.nj):
+                    der_net = derivate_sigmoid(layer.net(j))
+                    # invertire output???
+                    Dloss = der_loss(output_NN[j], output_expected[j])
+                    delta[j] = np.dot(Dloss, der_net)
+                    gradiente = np.dot(layer.x, delta[j])
+                    product = np.dot(learning_rate, gradiente)
+                    layer.w_matrix[:, j] = np.add(layer.w_matrix[:, j], product)
+
+                print(np.subtract(output_expected, output_NN))
+            else:
+                for j in range(layer.nj):
+                    der_net = derivate_sigmoid(layer.net(j))
+                    #print("vettore pesi nodo", j, struct_layers[i+1].w_matrix[:,j],
+                    #"vettore delta_out ", delta_out)
+                    product = np.dot(struct_layers[i+1].w_matrix[j,:],delta_out)
+                    delta[j] = np.sum(product)
+                    delta[j] = np.dot(delta[j], der_net)
+                    gradiente = np.dot(layer.x, delta[j])
+                    product = np.dot(learning_rate, delta[j])
+                    layer.w_matrix[:,j] = np.add(layer.w_matrix[:, j], product)
+
+            delta_out = delta
+
+    print(output_NN , output_expected)
