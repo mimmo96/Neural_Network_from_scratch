09c41451b13bd5d4ffc8dbed9b0a872b8a2a4bd2
Gerlando30 <gerlandogramaglia@hotmail.it>
2020-11-28 11:42:17 +0100

    Cambio cartelle

--- Bp.py
+++ Bp.py
@@ -1,7 +1,6 @@
 import numpy as np
 from function import output_nn, der_loss, derivate_sigmoid
 alfa = 0.9
-#v_lambda = 0.01
 
 def backprogation(struct_layers, num_epoch, learning_rate, x_input, output_expected):
     for epoch in range(0, num_epoch):
@@ -14,22 +13,21 @@ def backprogation(struct_layers, num_epoch, learning_rate, x_input, output_expec
             delta = np.zeros(layer.nj)
             for j in range(0, layer.nj):
 
-                if i == (np.size(struct_layers) - 1):
-                    delta[j] = der_loss(output_NN[j], output_expected[j])
+                der_net = derivate_sigmoid(layer.net(j))
 
+                if i == (np.size(struct_layers) - 1):
+                    Dloss = der_loss(output_NN[j], output_expected[j])
+                    delta[j] = np.dot(Dloss, der_net)
                 else:
-                    der_sig = derivate_sigmoid(layer.net(j))
                     #(delta_livello_successivo) * w del nodo_corrente(j) ai nodi successivi(k)
                     product = np.dot(struct_layers[i + 1].w_matrix[j, :], delta_out)
                     delta[j] = np.sum(product)
-                    delta[j] = np.dot(delta[j], der_sig)
+                    delta[j] = np.dot(delta[j], der_net)
 
-                gradient = np.dot(layer.x, delta[j])
-                Dw_new = np.dot(gradient, learning_rate)
+                gradiente = np.dot(layer.x, delta[j])
+                Dw_new = np.dot(gradiente, learning_rate)
                 #Dw_new = DeltaW_new(Dw_new, layer.Delta_w_old[:,j], epoch)
-                #tmp = layer.w_matrix[:, j]
                 layer.w_matrix[:, j] = np.add(layer.w_matrix[:, j], Dw_new)
-                #layer.w_matrix[:, j] = np.subtract(layer.w_matrix[:, j], np.dot(v_lambda,tmp))
                 #layer.Delta_w_old[:,j] = Dw_new
 
             delta_out = delta
