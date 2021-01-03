d444e23c874a3a0434c4b6917f0bae3bf19552e7
Gerlando30 <gerlandogramaglia@hotmail.it>
2021-01-03 17:32:51 +0100

    Model_selection

--- Model_selection.py
+++ Model_selection.py
@@ -1,5 +1,7 @@
 import neural_network
 import numpy as np
+from function import input_matrix, output_matrix
+from ThreadPool import ThreadPool
 def model_selection(vector_alfa, vector_learning_rate, vector_lambda, vectors_units, training_set, validation_set, batch_size, epochs):
     min = 0
     for alfa in vector_alfa:
@@ -12,7 +14,12 @@ def model_selection(vector_alfa, vector_learning_rate, vector_lambda, vectors_un
                     if min == 0:
                         min = tmp
                     else:
-                        min[1] < tmp[1]
-                        min = tmp
+                        if tmp[1] < min[1]: 
+                            min = tmp
 
-    print(min)
+    validation_set_input = input_matrix(validation_set)
+    validation_set_output = output_matrix(validation_set)
+    output_NN = np.zeros(validation_set_output.shape)
+    ThreadPool(min[0], validation_set_input, 0, validation_set_input.shape[0], output_NN)
+    print("best model ", output_NN)
+    
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import function
 import neural_network
 from Model_selection import model_selection
 layer=2
-learning_rate=0.03
+learning_rate=0.01
 num_epoch=1000
 filename = "dati.csv"
 batch_size=2
@@ -19,16 +19,16 @@ newInput=matriceinput[0]
 num_righe, num_colonne = matriceinput.shape;
 output_expected = matriceinput[:, (num_colonne-2):(num_colonne)]
 dim_input=np.size(newInput) - 2
-nj= [[dim_input,10,2,2,0]] #np.random.randint(1,5)
+nj= [[dim_input,5,2,2,0], [dim_input,10,2,2,0] ] #np.random.randint(1,5)
 #CORREGGERE MATRICEINPUT CON TR_INPUT
 #training_set_output = training_set[:, 0:training_set.shape[1] -2 ]
 
-alfa = [0.1, 0.5, 0.9]
-learning_rate = [0.01, 0.05, 0.09]
-v_lambda = [0.1, 0.001, 0.21]
+alfa = [0, 0.5, 0.9]
+learning_rate = [0.01, 0.05, 0.1]
+v_lambda = [0, 0.01, 0.1]
 
 model_selection(alfa, learning_rate, v_lambda, nj, training_set, validation_set, batch_size, num_epoch)
 
-neural_network1=neural_network.neural_network(nj, alfa, v_lambda, learning_rate, 3)
+#neural_network1=neural_network.neural_network(nj, alfa, v_lambda, learning_rate, 3)
 #neural_network.mini_batch()
-neural_network1.trainig(training_set, validation_set, batch_size, 100)
+#neural_network1.trainig(training_set, validation_set, batch_size, 100)
